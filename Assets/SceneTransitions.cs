7b3c81b3db1c3ed4a7797f41efb8301eb62d30c6
Tiffany Cheng <tacheng@umich.edu>
2024-10-12 23:29:48 -0400

    added trees + need finish button
    yes

---
+++ Assets/SceneTransitions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public string sceneName;
+
+    public void changeScene() {
+        SceneManager.LoadScene("Main VR Scene");
+    }
+
+}
+
